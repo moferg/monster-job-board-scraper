ec53476d7416b59a3ecaf2a70111b90673e7868d
Marshall Ferguson <moferg01@louisville.edu>
2020-09-02 03:14:30 -0400

    added a loop to output job titles

--- monster_job_board_scraper.py
+++ monster_job_board_scraper.py
@@ -5,17 +5,23 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-print('Welcome to the Monster Job Board Scraper!')
-print('This script with scrape the Monster website and return job postings.')
-job_title_input = input('What job title are you searching for?     ')
-location_input = input('What city are you looking to work in?     ')
+# print('Welcome to the Monster Job Board Scraper!')
+# print('This script with scrape the Monster website and return job postings.')
+# job_title_input = input('What job title are you searching for?     ')
+# location_input = input('What city are you looking to work in?     ')
 
-job_title_url = 'q=' + job_title_input
-location_url = '&where=' + location_input
+# job_title_url = 'q=' + job_title_input
+# location_url = '&where=' + location_input
 
-url = 'https://www.monster.com/jobs/search/?' + job_title_url + location_url
+# url = 'https://www.monster.com/jobs/search/?' + job_title_url + location_url
 # print(url)
+url = 'https://www.monster.com/jobs/search/?q=software-developer&where=louisville'     # This url is for testing purposes
 page = urlopen(url)
 html = page.read().decode('utf-8')
 soup = BeautifulSoup(html, 'html.parser')
-print(type(soup))
+# print(soup)
+job_titles_list = soup.select('.card-header a')
+# print(job_titles_list)
+for i in range(len(job_titles_list)):
+    job_title = job_titles_list[i].get_text()
+    print(job_title)
