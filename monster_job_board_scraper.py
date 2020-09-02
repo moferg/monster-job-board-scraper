094986041dee01b505d3b682c9f6e067dd7de2e7
Marshall Ferguson <moferg01@louisville.edu>
2020-09-02 02:31:08 -0400

    user can now input data to alter target url

--- monster_job_board_scraper.py
+++ monster_job_board_scraper.py
@@ -5,9 +5,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = 'https://www.monster.com/'
+print('Welcome to the Monster Job Board Scraper!')
+print('This script with scrape the Monster website and return job postings.')
+job_title_input = input('What job title are you searching for?     ')
+location_input = input('What city are you looking to work in?     ')
+
+job_title_url = 'q=' + job_title_input
+location_url = '&where=' + location_input
+
+url = 'https://www.monster.com/jobs/search/?' + job_title_url + location_url
+# print(url)
 page = urlopen(url)
 html = page.read().decode('utf-8')
 soup = BeautifulSoup(html, 'html.parser')
-
-print(soup)
+print(type(soup))
