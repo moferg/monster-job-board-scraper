47abd4b309c84f2a9b2b9fad18f8c4db3e67cd33
Marshall Ferguson <moferg01@louisville.edu>
2020-09-02 02:17:50 -0400

    added and tested url request

--- monster_job_board_scraper.py
+++ monster_job_board_scraper.py
@@ -1,1 +1,13 @@
 # Marshall Ferguson - Monster Job Board Scraper - 8/2020
+
+# Imports
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = 'https://www.monster.com/'
+page = urlopen(url)
+html = page.read().decode('utf-8')
+soup = BeautifulSoup(html, 'html.parser')
+
+print(soup)
